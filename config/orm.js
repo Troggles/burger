fecdcb53ea6d8f999d2ba5e55b3af8b201818f6a
Adam Griepentrog <adammiltron@gmail.com>
2018-10-14 13:24:03 -0500

    ORM push

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,40 @@
 const connection = require("../config/connection.js");
+
+var orm = {
+    selectALL: function (cb) {
+    var queryString = "SELECT * FROM burgers";
+    connection.query(queryString, function (err,result){
+        if(err) {
+            throw err;
+        }
+        cb(result);
+    });
+},
+
+insertOne: function (burger, cb) {
+    var queryString = "INSERT INTO BURGERS (burger_name VALUES (?)";
+    connection.query(queryString, [burger], function(err, result) {
+        if (err) {
+            throw err;
+        }
+        cb(result); 
+    });
+},
+
+updateOne: function (id, cb) {
+    var queryString = "UPDATE burgers SET devoured = true WHERE id = ?";
+    
+    connection.query(queryString, [id], function (err,result) {
+        if(err) {
+            throw err;
+        }
+        cb(result);
+    });
+}     
+
+
+};
+
+module.exports = orm; 
+
+
