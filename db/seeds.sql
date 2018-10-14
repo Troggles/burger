c7236cff8141f0a26c59ba2c1209f3e9c21a8363
Adam Griepentrog <adammiltron@gmail.com>
2018-10-13 22:04:56 -0500

    inital file setup for connection orm databases

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('TripleByass Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quadruple Royale with Cheese', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', FALSE);
+
+
+
+
