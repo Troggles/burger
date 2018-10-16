f9def89184a9484b1bdb795a26c36982bafd103b
Adam Griepentrog <adamgriepentrog@Adams-iMac.local>
2018-10-16 14:06:25 -0500

    fixed connection script

--- config/connection.js
+++ config/connection.js
@@ -9,9 +9,11 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
+        port: 3306,
         user: 'root',
         password: '',
-        database: 'burgers_db'
+        database: 'burgers_db',
+        
     });
 
 }
